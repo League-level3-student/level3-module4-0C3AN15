25052e4b2eea98082a8a9d2006ad2e7a6e30d77a
league <karinabriellen@gmail.com>
2024-03-01 18:26:09 -0800

    progress

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,9 +1,20 @@
 package _03_Hangman;
 
+import java.util.Stack;
+
 import javax.swing.JOptionPane;
 
 public class Hangman {
-
-	JOptionPane.showInputDialog("Number:");
 	
+	public static void main(String[] args) {
+		
+	String numb = JOptionPane.showInputDialog("Number:");
+	int num = Integer.parseInt(numb);
+	Stack<String> game = new Stack<String>();
+	
+	for(int i=0;i<num;i++) {
+		System.out.println(game.push(Utilities.readRandomLineFromFile("dictionary.txt")));
+	}
+	
+	}
 }
