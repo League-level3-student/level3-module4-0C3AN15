989ec7e45dac99691d35089bf1d97d74624db352
lvl0_module2 <zoey.lestyk@gmail.com>
2024-03-08 18:27:28 -0800

    progress v0

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,20 +1,54 @@
 package _03_Hangman;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
 import java.util.Stack;
 
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
-public class Hangman {
+public class Hangman implements KeyListener {
 	
-	public static void main(String[] args) {
-		
 	String numb = JOptionPane.showInputDialog("Number:");
 	int num = Integer.parseInt(numb);
 	Stack<String> game = new Stack<String>();
+	
+	Random rand = new Random();
+	JLabel label = new JLabel();
+	int lives = 3;
+	String word;
+	int wordLong;
+	
+	public void main(String[] args) {
 	
 	for(int i=0;i<num;i++) {
 		System.out.println(game.push(Utilities.readRandomLineFromFile("dictionary.txt")));
 	}
 	
+	word = game.pop();
+	wordLong = word.length();
+	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		String key = arg0.toString();
+		if(word.contains(key)) {
+			
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
